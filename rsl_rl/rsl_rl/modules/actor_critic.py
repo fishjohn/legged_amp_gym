4749350edd639d7c5ae59545fa7a3106b63cecee
fishjohn <1258676867@qq.com>
2023-08-27 20:06:32 +0800

    amp work with low amp reward

--- rsl_rl/rsl_rl/modules/actor_critic.py
+++ rsl_rl/rsl_rl/modules/actor_critic.py
@@ -62,6 +62,7 @@ class ActorCritic(nn.Module):
         for l in range(len(actor_hidden_dims)):
             if l == len(actor_hidden_dims) - 1:
                 actor_layers.append(nn.Linear(actor_hidden_dims[l], num_actions))
+                actor_layers.append(get_activation('tanh'))
             else:
                 actor_layers.append(nn.Linear(actor_hidden_dims[l], actor_hidden_dims[l + 1]))
                 actor_layers.append(activation)
